2ffe82ad61fa2dab663b8b764d243ca0ad573b65
Stronkhorst, Jonathan (AV) <JStronkhorst@svb.nl>
2023-07-04 14:57:19 +0200

    Adding mappings for Link and Comment.

--- src/main/java/com/jstronkhorst/springit/domain/Comment.java
+++ src/main/java/com/jstronkhorst/springit/domain/Comment.java
@@ -3,8 +3,10 @@ package com.jstronkhorst.springit.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Data
@@ -14,5 +16,9 @@ public class Comment {
     @Id
     @GeneratedValue
     private long id;
+    @NonNull
     private String body;
+
+    @ManyToOne
+    private Link link;
 }
--- src/main/java/com/jstronkhorst/springit/domain/Link.java
+++ src/main/java/com/jstronkhorst/springit/domain/Link.java
@@ -3,8 +3,13 @@ package com.jstronkhorst.springit.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -14,6 +19,11 @@ public class Link {
     @Id
     @GeneratedValue
     private Long id;
+    @NonNull
     private String title;
+    @NonNull
     private String url;
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 }
