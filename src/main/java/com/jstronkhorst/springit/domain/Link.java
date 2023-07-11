cbca530b9834f4385fe4bfd3988a581d744afea3
Stronkhorst, Jonathan (AV) <JStronkhorst@svb.nl>
2023-07-11 16:22:35 +0200

    DMS-7999 Springboot 2 Course in Oreilly
    In this branch I added templates and pages to view a list of the
    links, links by themselfs and a page to submit links. Using the
    thymeleaf framework

--- src/main/java/com/jstronkhorst/springit/domain/Link.java
+++ src/main/java/com/jstronkhorst/springit/domain/Link.java
@@ -1,12 +1,21 @@
 package com.jstronkhorst.springit.domain;
 
+import com.jstronkhorst.springit.service.BeanUtil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
+import org.ocpsoft.prettytime.PrettyTime;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -19,9 +28,14 @@ public class Link extends Auditable {
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
+    @NotEmpty(message = "Please enter a title.")
     private String title;
+
     @NonNull
+    @NotEmpty(message = "Please enter a url.")
+    @URL(message = "Please enter a valid url.")
     private String url;
 
     @OneToMany(mappedBy = "link")
@@ -29,5 +43,20 @@ public class Link extends Auditable {
 
     public void addComment(Comment comment) {
         comments.add(comment);
+    }
+
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreatedDateTime()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
